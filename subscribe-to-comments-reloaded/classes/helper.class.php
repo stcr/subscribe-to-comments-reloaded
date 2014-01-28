6a60fd8735393f72df6dfd2c010424654c5d7d24
Raam Dev <raam@raamdev.com>
2014-01-28 12:05:53 -0500

    Reformat code to comply with WordPress coding standards

--- subscribe-to-comments-reloaded/classes/helper.class.php
+++ subscribe-to-comments-reloaded/classes/helper.class.php
@@ -1,32 +1,36 @@
 <?php
+
 /**
-*	@author 	Ing. Israel Barragan C.  Email: ibarragan@behstant.com
-*	@since 		10-Jul-2013
-*	##########################################################################################
-*	Comments:
-*	This class handle functions or methos that will be helping the plugin Subscribe to Comments Reloaded
-* 	Plugin URI: http://wordpress.org/extend/plugins/subscribe-to-comments-reloaded/
-*	##########################################################################################
-*	@version
-*	##########################################################################################
-*	1.0		|	10-Jul-2013	|	Creation of new class. Adding the method verifyXSS().
-*	V140109 |	09-Jan-2013	|	* Rename the class name. The name was in conflict with another class.
-*	##########################################################################################
-*/
+ *
+ *
+ * @author       Ing. Israel Barragan C.  Email: ibarragan@behstant.com
+ * @since        10-Jul-2013
+ *    ##########################################################################################
+ *    Comments:
+ *    This class handle functions or methos that will be helping the plugin Subscribe to Comments Reloaded
+ *    Plugin URI: http://wordpress.org/extend/plugins/subscribe-to-comments-reloaded/
+ *    ##########################################################################################
+ * @version
+ *    ##########################################################################################
+ *    1.0        |    10-Jul-2013    |    Creation of new class. Adding the method verifyXSS().
+ *    V140109 |    09-Jan-2013    |    * Rename the class name. The name was in conflict with another class.
+ *    ##########################################################################################
+ */
 class subscribeToCommentsHelper {
 
-	function __construct()
-	{
+	function __construct() {
 
 	}
 
-	function verifyXSS($value){
-		$pattern = '~(<|<script>|</|</script>|(%3C|%3C/))~';
+	function verifyXSS( $value ) {
+		$pattern  = '~(<|<script>|</|</script>|(%3C|%3C/))~';
 		$detected = false;
-		if (preg_match($pattern, $value)) {
+		if ( preg_match( $pattern, $value ) ) {
 			$detected = true;
 		}
+
 		return $detected;
 	}
 }
+
 ?>
