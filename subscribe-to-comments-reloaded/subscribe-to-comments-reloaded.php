73fa6b9b81b5f50f4c7299d3ec90a0c102fc4acc
Reedyseth <reedyseth@gmail.com>
2014-10-24 10:11:31 -0600

    Fix data overflow when the comment reply column is not available and when the plugin was activated.

--- subscribe-to-comments-reloaded/subscribe-to-comments-reloaded.php
+++ subscribe-to-comments-reloaded/subscribe-to-comments-reloaded.php
@@ -294,7 +294,7 @@ class wp_subscribe_reloaded {
 		add_option( 'subscribe_reloaded_author_label', __( "You can <a href='[manager_link]'>manage the subscriptions</a> of this post.", 'subscribe-reloaded' ), '', 'no' );
 
 		add_option( 'subscribe_reloaded_manager_page_enabled', 'yes', '', 'no' );
-		add_option( 'subscribe_reloaded_virtual_manager_page_enabled','yes', '', 'no' );
+		add_option( 'subscribe_reloaded_virtual_manager_page_enabled', 'yes', '', 'no' );
 		add_option( 'subscribe_reloaded_manager_page_title', __( 'Manage subscriptions', 'subscribe-reloaded' ), '', 'no' );
 		add_option( 'subscribe_reloaded_custom_header_meta', "<meta name='robots' content='noindex,nofollow'>", '', 'no' );
 		add_option( 'subscribe_reloaded_request_mgmt_link', __( 'To manage your subscriptions, please enter your email address here below. We will send you a message containing the link to access your personal management page.', 'subscribe-reloaded' ), '', 'no' );
@@ -1396,34 +1396,30 @@ class wp_subscribe_reloaded {
 	 */
 	private function _import_crn_data() {
 		global $wpdb;
+		$crn_data_count          = null;
 		$subscriptions_to_import = array();
-		// THIS QUERY wil make sure that the columns exist
-		/*  SHOW COLUMNS FROM wp_comments LIKE 'comment_mail_notify';
-				other one:
-		SELECT *
-		FROM information_schema.COLUMNS
-		WHERE
-			TABLE_SCHEMA = 'db_name'
-		AND TABLE_NAME = 'table_name'
-		AND COLUMN_NAME = 'column_name'
-		*/
-		// Import the information collected by Subscribe to Comments, if needed
-		$crn_data_count = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->comments WHERE comment_mail_notify = 1" );
+		$commentMailColumn       = $wpdb->get_var( "SHOW COLUMNS FROM $wpdb->comments LIKE 'comment_mail_notify' " );
+		if ( empty( $commentMailColumn ) ) {
+			$crn_data_count = 0;
+		} else {
+			// Import the information collected by Subscribe to Comments, if needed
+			$crn_data_count = $wpdb->get_var( "SELECT COUNT(*) FROM $wpdb->comments WHERE comment_mail_notify = 1" );
+		}
 
-		if ( $crn_data_count != null && $crn_data_count > 0 ) { // if $crn_data_count is null there is no Comment Reply
-																// plugin installed and therefore no comment_mail_notify
-																// column.
-			// 1) If there are subscriptions Retrieve all of them from COMMENT_REPLY_NOTIFICATION
+		if ( $crn_data_count > 0 ) { // if $crn_data_count is 0 there is no Comment Reply
+			// plugin installed and therefore no comment_mail_notify
+			// column.
+			// Since we know that there are subscriptions Retrieve all of them from COMMENT_REPLY_NOTIFICATION
 			$crn_data             = $wpdb->get_results(
-									 " SELECT comment_post_ID, comment_author_email"
-									." FROM wp_comments WHERE comment_mail_notify = '1'"
-									." GROUP BY comment_author_email"
-									, OBJECT
+				" SELECT comment_post_ID, comment_author_email"
+				. " FROM wp_comments WHERE comment_mail_notify = '1'"
+				. " GROUP BY comment_author_email"
+				, OBJECT
 			);
 			$stcr_data            = $wpdb->get_results(
-									" SELECT post_id, SUBSTRING(meta_key,8) AS email"
-									."FROM wp_postmeta WHERE meta_key LIKE '_stcr@_%'"
-								 	, ARRAY_N
+				" SELECT post_id, SUBSTRING(meta_key,8) AS email"
+				. "FROM wp_postmeta WHERE meta_key LIKE '_stcr@_%'"
+				, ARRAY_N
 			);
 			$sctr_data_array_size = sizeof( $stcr_data );
 			// Lets make sure that there is not another subscription with the same compose key
