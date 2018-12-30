f64659ec70351e00325e53db5ecbddfce873de18
reedyseth <reedyseth@gmail.com>
2018-12-30 01:00:56 -0700

    Fix value of management page to get it on the event insteadof page load.

--- src/includes/js/admin/management_page.js
+++ src/includes/js/admin/management_page.js
@@ -4,18 +4,23 @@
 ( function($){
     $(document).ready(function(){
 
-        var option_manager_page = $("input[name='options[manager_page]']" ).val();
+        var option_manager_page = $("input[name='options[manager_page]']" );
         var missing_fields = [];
 
         // management_page_form
         $('form.management_page_form').submit(function(event){
-            if( option_manager_page === "" )
+            if( option_manager_page.val() === "" )
             {
-                missing_fields.push(
+            	var field = "options[manager_page]";
+
+            	if( ! checkArrayKey( missing_fields, field ) )
+            	{
+            		missing_fields.push( // TODO: Only push if the array does not contain the key already.
                     {
                         message: "<?php _e( 'Missing information', 'subscribe-reloaded' ) ?>",
-                        field: "options[manager_page]"
+                        field: field
                     } );
+            	}
             }
 
             var missing_fields_size = missing_fields.length;
@@ -30,10 +35,27 @@
                     // $("form#add_new_subscription .validate-error-text-" + field_obj.field).text(field_obj.message).show();
                     $("form.management_page_form input[name='"+ field_obj.field +"']").addClass("validate-error-field");
                     $('html, body').animate({scrollTop:0}, 'slow');
+
+                    missing_fields = []; // clean house
                     return false;
                 }
 
             }
         });
+
+
+        function checkArrayKey( arrayObj, keyToCheck )
+		{
+			var size = arrayObj.length;
+
+			for(var i = 0; i < size; i++)
+			{
+				if( arrayObj[i].field == keyToCheck )
+				{
+					return true;
+				}
+			}
+			return false;
+		}
     });
 } )( jQuery );
