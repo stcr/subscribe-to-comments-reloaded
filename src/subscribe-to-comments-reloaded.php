d139c351ca05ef20dd66adb6740cc37d625ac10f
Slobodan Kustrimovic <skustrimovic@gmail.com>
2019-05-10 08:00:37 +0200

    Correct text domain

--- src/subscribe-to-comments-reloaded.php
+++ src/subscribe-to-comments-reloaded.php
@@ -7,7 +7,7 @@
  * Author URI:        http://wpkube.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       subscribe-reloaded
+ * Text Domain:       subscribe-to-comments-reloaded
  * Domain Path:       /langs
  */
 
