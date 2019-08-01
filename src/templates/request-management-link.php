969dbd849b8ca6d0cf9ef6916c6d086e7ca1174d
Slobodan Kustrimovic <skustrimovic@gmail.com>
2019-08-01 17:17:43 +0200

    cleanup request management link template

--- src/templates/request-management-link.php
+++ src/templates/request-management-link.php
@@ -1,9 +1,11 @@
 <?php
+
 // Avoid direct access to this piece of code
 if ( ! function_exists( 'add_action' ) ) {
     header( 'Location: /' );
     exit;
 }
+
 global $wp_subscribe_reloaded;
 
 // The the page where the user is coming from
@@ -12,32 +14,32 @@ $current_user_email = null; // Comes from wp_subscribe-to-comments-reloaded\subs
 $ID = $target_post;
 $valid_email = true;
 
-if ( isset($current_user) && $current_user->ID > 0 )
-{
+// get email if user known
+if ( isset( $current_user ) && $current_user->ID > 0 ) {
     $current_user_email = $current_user->data->user_email;
 }
 
-if (array_key_exists('post_permalink', $_GET))
-{
-    if ( ! empty( $_GET['post_permalink'] ) )
-    {
+// post permalink supplied with $_GET
+if ( array_key_exists('post_permalink', $_GET ) ) {
+    if ( ! empty( $_GET['post_permalink'] ) ) {
         $post_permalink = $_GET['post_permalink'];
     }
 }
 
-
+// start output buffering
 ob_start();
 
+// email address not supplied
 if ( ! empty( $email ) ) {
 
     $stcr_post_email     = $wp_subscribe_reloaded->stcr->utils->check_valid_email( $email );
 
-    if ( $stcr_post_email === false )
-    {
+    if ( $stcr_post_email === false ) {
+        
         $valid_email = false;
-    }
-    else
-    {
+
+    } else {
+        
         // Send management link
         $subject        = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_management_subject', 'Manage your subscriptions on [blog_name]' ) ), ENT_QUOTES, 'UTF-8' );
         $page_message   = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_management_content', '' ) ), ENT_QUOTES, 'UTF-8' );
@@ -59,10 +61,8 @@ if ( ! empty( $email ) ) {
             . "&srk=$subscriber_salt" . "&sra=u;srsrc=e" . "&srp=";
 
         // Replace tags with their actual values
-        $subject       = str_replace( '[blog_name]', get_bloginfo( 'name' ), $subject );
-        // Setup the fronted page message
-        $page_message  = str_replace( '[blog_name]', get_bloginfo( 'name' ), $page_message );
-        // Setup the email message
+        $subject = str_replace( '[blog_name]', get_bloginfo( 'name' ), $subject );
+        $page_message = str_replace( '[blog_name]', get_bloginfo( 'name' ), $page_message );
         $email_message = str_replace( '[blog_name]', get_bloginfo( 'name' ), $email_message );
         $email_message = str_replace( '[manager_link]',  $manager_link, $email_message );
         $email_message = str_replace( '[oneclick_link]', $one_click_unsubscribe_link, $email_message );
@@ -74,38 +74,40 @@ if ( ! empty( $email ) ) {
             $page_message  = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $page_message );
             $email_message = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $email_message );
         }
+
         // Prepare email settings
         $email_settings = array(
             'subject'      => $subject,
             'message'      => $email_message,
             'toEmail'      => $clean_email
         );
+
         $wp_subscribe_reloaded->stcr->utils->send_email( $email_settings );
 
         echo wpautop( $page_message );
-    }
-}
-else
-{
-    $message = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_request_mgmt_link' ) ), ENT_QUOTES, 'UTF-8' );
-    $email = '';
 
-    if ( isset($current_user_email) )
-    {
+    }
+
+// email address supplied
+} else {
+
+    $message = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_request_mgmt_link' ) ), ENT_QUOTES, 'UTF-8' );
+    
+    // get email address
+    $email = '';
+    if ( isset($current_user_email) ) {
         $email = $current_user_email;
-    }
-    else if ( isset( $_COOKIE['comment_author_email_' . COOKIEHASH] ))
-    {
+    } else if ( isset( $_COOKIE['comment_author_email_' . COOKIEHASH] )) {
         $email = sanitize_email( $_COOKIE['comment_author_email_' . COOKIEHASH] );
-    }
-    else
-    {
+    } else {
         $email = 'email';
     }
 
+    // qTrans compatibility
     if ( function_exists( 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage' ) ) {
         $message = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $message );
     }
+
     ?>
     <p><?php echo wpautop( $message ); ?></p>
     <form action="<?php echo esc_url( $_SERVER[ 'REQUEST_URI' ]);?>" method="post" name="sub-form">
@@ -119,24 +121,29 @@ else
     </form>
     <?php
 
-    if ( isset( $post_permalink ) )
-    {
+    if ( isset( $post_permalink ) ) {
         echo '<p id="subscribe-reloaded-update-p"> 
             <a style="margin-right: 10px; text-decoration: none; box-shadow: unset;" href="'. esc_url( $post_permalink ) .'"><i class="fa fa-arrow-circle-left fa-2x" aria-hidden="true" style="vertical-align: middle;"></i>&nbsp; '. __('Return to Post','subscribe-to-comments-reloaded').'</a>
           </p>';
     }
 
 }
-if( ! $valid_email )
-{
+
+// email invalid
+if( ! $valid_email ) {
+
     $message = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_request_mgmt_link' ) ), ENT_QUOTES, 'UTF-8' );
+    
     if ( function_exists( 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage' ) ) {
         $message = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $message );
     }
-    echo "<p> ". wpautop( $message ) . "</p>";?>
+
+    ?>
+    <p><?php echo wpautop( $message ); ?></p>
     <form action="<?php echo esc_url( $_SERVER[ 'REQUEST_URI' ]);?>" method="post" name="sub-form">
         <fieldset style="border:0">
-            <p><label for="subscribe_reloaded_email"><?php _e( 'Email', 'subscribe-to-comments-reloaded' ) ?></label>
+            <p>
+                <label for="subscribe_reloaded_email"><?php _e( 'Email', 'subscribe-to-comments-reloaded' ) ?></label>
                 <input id='subscribe_reloaded_email' type="text" class="subscribe-form-field" name="sre" value="<?php echo esc_attr( $email ); ?>" size="22"  />
                 <input name="submit" type="submit" class="subscribe-form-button" value="<?php _e( 'Send', 'subscribe-to-comments-reloaded' ) ?>" />
             </p>
@@ -145,6 +152,8 @@ if( ! $valid_email )
     </form>
     <?php
 }
+
+// output script
 ?>
     <script type="text/javascript">
         ( function($){
@@ -189,8 +198,8 @@ if( ! $valid_email )
         } )( jQuery );
     </script>
 <?php
+// stop output buffer and pass it back
 $output = ob_get_contents();
 ob_end_clean();
-
 return $output;
 ?>
