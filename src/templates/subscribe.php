1aa1685ab1392ad03f9d4f12d427f0bb493ba900
Slobodan Kustrimovic <skustrimovic@gmail.com>
2019-08-01 14:06:16 +0200

    clean up subscribe template

--- src/templates/subscribe.php
+++ src/templates/subscribe.php
@@ -1,4 +1,5 @@
 <?php
+
 // Avoid direct access to this piece of code
 if ( ! function_exists( 'add_action' ) ) {
     header( 'Location: /' );
@@ -10,25 +11,34 @@ global $wp_subscribe_reloaded;
 $current_user_email = null; // Comes from wp_subscribe-to-comments-reloaded\subscribe_reloaded_manage()
 $valid_email = true;
 
-if ( isset($current_user) && $current_user->ID > 0 )
-{
+// get user email
+if ( isset($current_user) && $current_user->ID > 0 ) {
     $current_user_email = $current_user->data->user_email;
 }
 
-ob_start();
+// get post permalink
 $post_permalink = get_permalink( $post_ID );
 
-if ( ! empty( $email ) ) {
-    $stcr_post_email     = $wp_subscribe_reloaded->stcr->utils->check_valid_email( $email );
+// start output buffer
+ob_start();
 
-    if ( $stcr_post_email === false )
-    {
+// email address supplied
+if ( ! empty( $email ) ) {
+    
+    // check email validity
+    $stcr_post_email = $wp_subscribe_reloaded->stcr->utils->check_valid_email( $email );
+    
+    // email is invalid
+    if ( $stcr_post_email === false ) {
+        
         $valid_email = false;
-    }
-    else
-    {
+
+    // email is valid
+    } else {
+
         // Use Akismet, if available, to check this user is legit
         if ( function_exists( 'akismet_http_post' ) ) {
+
             global $akismet_api_host, $akismet_api_port;
 
             $akismet_query_string  = "user_ip={$_SERVER['REMOTE_ADDR']}";
@@ -44,41 +54,46 @@ if ( ! empty( $email ) ) {
             // If this is considered SPAM, we stop here
             if ( $akismet_response[1] == 'true' ) {
                 ob_end_clean();
-
                 return '';
             }
+
         }
 
+        // sanitize email address
         $clean_email = $wp_subscribe_reloaded->stcr->utils->clean_email( $email );
 
-        // If the case, send a message to the administrator
-        if ( get_option( 'subscribe_reloaded_enable_admin_messages' ) == 'yes' )
-        {
+        // notify the administrator about the new subscription
+        if ( get_option( 'subscribe_reloaded_enable_admin_messages' ) == 'yes' ) {
+            
             $from_name  = stripslashes( get_option( 'subscribe_reloaded_from_name', 'admin' ) );
             $from_email = get_option( 'subscribe_reloaded_from_email', get_bloginfo( 'admin_email' ) );
 
             $subject = __( 'New subscription to', 'subscribe-to-comments-reloaded' ) . " $target_post->post_title";
             $message = __( 'New subscription to', 'subscribe-to-comments-reloaded' ) . " $target_post->post_title\n" . __( 'User:', 'subscribe-to-comments-reloaded' ) . " $clean_email";
-            // Prepare email settings
+            
             $email_settings = array(
                 'subject'      => $subject,
                 'message'      => $message,
                 'toEmail'      => get_bloginfo( 'admin_email' )
             );
+            
             $wp_subscribe_reloaded->stcr->utils->send_email( $email_settings );
+
         }
-        if ( get_option( 'subscribe_reloaded_enable_double_check' ) == 'yes'
-            && ! $wp_subscribe_reloaded->stcr->is_user_subscribed( $post_ID, $clean_email, 'C' ) )
-        {
+
+        // double check, send confirmation email
+        if ( get_option( 'subscribe_reloaded_enable_double_check' ) == 'yes' && ! $wp_subscribe_reloaded->stcr->is_user_subscribed( $post_ID, $clean_email, 'C' ) ) {
             $wp_subscribe_reloaded->stcr->add_subscription( $post_ID, $clean_email, 'YC' );
             $wp_subscribe_reloaded->stcr->confirmation_email( $post_ID, $clean_email );
             $message = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_subscription_confirmed_dci' ) ), ENT_QUOTES, 'UTF-8' );
-        }
-        else {
+        
+        // not double check, add subscription
+        } else {
             $this->add_subscription( $post_ID, $clean_email, 'Y' );
             $message = html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_subscription_confirmed' ) ), ENT_QUOTES, 'UTF-8' );
         }
 
+        // new subscription message
         $message = str_replace( '[post_permalink]', $post_permalink, $message );
         if ( function_exists( 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage' ) ) {
             $message = str_replace( '[post_title]', qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $target_post->post_title ), $message );
@@ -86,24 +101,24 @@ if ( ! empty( $email ) ) {
         } else {
             $message = str_replace( '[post_title]', $target_post->post_title, $message );
         }
-        echo wpautop($message);
+
+        echo wpautop( $message );
+
     }
-}
-else {
-    if ( isset($current_user_email) )
-    {
+
+// no email address supplied
+} else {
+
+    // email value for input field
+    if ( isset( $current_user_email ) ) {
         $email = $current_user_email;
-    }
-    else if ( isset( $_COOKIE['comment_author_email_' . COOKIEHASH] ))
-    {
+    } else if ( isset( $_COOKIE['comment_author_email_' . COOKIEHASH] )) {
         $email = sanitize_email( $_COOKIE['comment_author_email_' . COOKIEHASH] );
-    }
-    else
-    {
+    } else {
         $email = 'email';
     }
 
-    echo "<p>";
+    // output message for subscribing without commenting
     $message = str_replace( '[post_permalink]', $post_permalink, html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_subscribe_without_commenting' ) ), ENT_QUOTES, 'UTF-8' ) );
     if ( function_exists( 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage' ) ) {
         $message = str_replace( '[post_title]', qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $target_post->post_title ), $message );
@@ -111,25 +126,28 @@ else {
     } else {
         $message = str_replace( '[post_title]', $target_post->post_title, $message );
     }
-    echo $message;
-    echo "</p>";
+    echo '<p>' . $message . '</p>';
+
+    // output the form
     ?>
-    <form action="<?php
-    echo esc_url( $_SERVER[ 'REQUEST_URI' ]);?>"
-          method="post" name="sub-form">
+    <form action="<?php echo esc_url( $_SERVER[ 'REQUEST_URI' ]); ?>" method="post" name="sub-form">
         <fieldset style="border:0">
-            <div><label for="sre"><?php _e( 'Email', 'subscribe-to-comments-reloaded' ) ?></label>
+            <div>
+                <label for="sre"><?php _e( 'Email', 'subscribe-to-comments-reloaded' ) ?></label>
                 <input id='sre' type="text" class="subscribe-form-field" name="sre" value="<?php echo esc_attr( $email ); ?>" size="22" />
                 <input name="submit" type="submit" class="subscribe-form-button" value="<?php _e( 'Send', 'subscribe-to-comments-reloaded' ) ?>" />
-            <p class="notice-email-error" style='color: #f55252;font-weight:bold; display: none;'></p>
+                <p class="notice-email-error" style='color: #f55252;font-weight:bold; display: none;'></p>
             </div>
         </fieldset>
     </form>
     <?php
+
 }
-if( ! $valid_email )
-{
-    echo "<p>";
+
+// invalid email
+if ( ! $valid_email ) {
+
+    // message for subscribing without commenting
     $message = str_replace( '[post_permalink]', $post_permalink, html_entity_decode( stripslashes( get_option( 'subscribe_reloaded_subscribe_without_commenting' ) ), ENT_QUOTES, 'UTF-8' ) );
     if ( function_exists( 'qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage' ) ) {
         $message = str_replace( '[post_title]', qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage( $target_post->post_title ), $message );
@@ -137,9 +155,8 @@ if( ! $valid_email )
     } else {
         $message = str_replace( '[post_title]', esc_html( $target_post->post_title ), $message );
     }
-    echo $message;
+    echo '<p>' . $message . '</p>';
 
-    echo "</p>";
     ?>
     <form action="<?php echo esc_url( $_SERVER[ 'REQUEST_URI' ]);?>" method="post" name="sub-form">
         <fieldset style="border:0">
@@ -151,52 +168,47 @@ if( ! $valid_email )
         </fieldset>
     </form>
     <?php
+
 }
+
 ?>
-    <script type="text/javascript">
-        ( function($){
-            $(document).ready(function($){
-                var stcr_request_form = $('form[name="sub-form"]');
-                var email_input       = $('form[name="sub-form"] input[name="sre"]');
-                /**
-                 * Validate the email address.
-                 * @since 09-Sep-2016
-                 * @author reedyseth
-                 */
-                stcr_request_form.on('submit',function (event) {
-                    var emailRegex   = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                    var email = $('input[name="sre"]');
+<script type="text/javascript">
+    ( function($){
+        $(document).ready(function($){
+            
+            var stcr_request_form = $('form[name="sub-form"]');
+            var email_input       = $('form[name="sub-form"] input[name="sre"]');           
 
-                    if( email.val() !== "email" && email.val() === "" )
-                    {
-                        event.preventDefault();
-                        $(".notice-email-error").html("<i class=\"fa fa-exclamation-triangle\"></i> <?php _e("Please enter your email", 'subscribe-to-comments-reloaded') ?>").show().delay(4000).fadeOut(1000);
-                    }
-                    else if( emailRegex.test( email.val() ) === false )
-                    {
-                        event.preventDefault();
-                        $(".notice-email-error").html("<i class=\"fa fa-exclamation-triangle\"></i> <?php _e("Email address is not valid", 'subscribe-to-comments-reloaded') ?>").show().delay(4000).fadeOut(1000);
-                    }
-                });
+            stcr_request_form.on('submit',function (event) {
+                var emailRegex   = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                var email = $('input[name="sre"]');
 
-                email_input.focus(function(){
-                    if( $(this).val() == <?php echo wp_json_encode( $email ); ?> )
-                    {
-                        $(this).val("");
-                    }
-                });
-
-                email_input.blur(function(){
-                    if( $(this).val() == "" )
-                    {
-                        $(this).val(<?php echo wp_json_encode( $email ); ?>);
-                    }
-                });
+                if( email.val() !== "email" && email.val() === "" ) {
+                    event.preventDefault();
+                    $(".notice-email-error").html("<i class=\"fa fa-exclamation-triangle\"></i> <?php _e("Please enter your email", 'subscribe-to-comments-reloaded') ?>").show().delay(4000).fadeOut(1000);
+                } else if( emailRegex.test( email.val() ) === false ) {
+                    event.preventDefault();
+                    $(".notice-email-error").html("<i class=\"fa fa-exclamation-triangle\"></i> <?php _e("Email address is not valid", 'subscribe-to-comments-reloaded') ?>").show().delay(4000).fadeOut(1000);
+                }
             });
-        } )( jQuery );
-    </script>
+
+            email_input.focus(function(){
+                if( $(this).val() == <?php echo wp_json_encode( $email ); ?> ) {
+                    $(this).val("");
+                }
+            });
+
+            email_input.blur(function(){
+                if( $(this).val() == "" ) {
+                    $(this).val(<?php echo wp_json_encode( $email ); ?>);
+                }
+            });
+        });
+    } )( jQuery );
+</script>
 
 <?php
+// stop output buffer and pass it back
 $output = ob_get_contents();
 ob_end_clean();
 return $output;
