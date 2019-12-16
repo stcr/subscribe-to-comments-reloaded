b967b9c7759534d0469454eb9c08da28621a5248
Slobodan Kustrimovic <skustrimovic@gmail.com>
2019-12-16 20:42:49 +0100

    Version bump 191216

--- src/wp_subscribe_reloaded.php
+++ src/wp_subscribe_reloaded.php
@@ -8,7 +8,7 @@ if ( ! function_exists( 'add_action' ) ) {
 }
 
 // globals
-define( __NAMESPACE__.'\\VERSION','191209' );
+define( __NAMESPACE__.'\\VERSION','191216' );
 define( __NAMESPACE__.'\\DEVELOPMENT', false );
 define( __NAMESPACE__.'\\SLUG', "subscribe-to-comments-reloaded" );
 
