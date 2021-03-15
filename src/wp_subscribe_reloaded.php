6258f1392fc734d7972438bdaed5bf1a5c3717a1
Slobodan Kustrimovic <skustrimovic@gmail.com>
2021-03-15 17:12:47 +0100

    Version bump 210315

--- src/wp_subscribe_reloaded.php
+++ src/wp_subscribe_reloaded.php
@@ -8,7 +8,7 @@ if ( ! function_exists( 'add_action' ) ) {
 }
 
 // globals
-define( __NAMESPACE__.'\\VERSION','210126' );
+define( __NAMESPACE__.'\\VERSION','210315' );
 define( __NAMESPACE__.'\\DEVELOPMENT', false );
 define( __NAMESPACE__.'\\SLUG', "subscribe-to-comments-reloaded" );
 
