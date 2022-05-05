12815dee3db8a4c41fd0cad6b7060e7a63b88faf
Bishal Napit <mebishalnapit@gmail.com>
2022-05-05 15:41:49 +0545

    Fix - Options save data not working as intended due to sanitization

--- src/options/stcr_options.php
+++ src/options/stcr_options.php
@@ -95,7 +95,6 @@ if ( array_key_exists( "generate_key", $_POST ) ) {
 
     $faulty_fields     = array();
     $subscribe_options = wp_unslash( $_POST['options'] ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput
-    $subscribe_options = array_map( 'sanitize_text_field', $subscribe_options );
     foreach ( $subscribe_options as $option => $value )
     {
         if ( ! $wp_subscribe_reloaded->stcr->utils->stcr_update_menu_options( $option, $value, $options[$option] ) )
