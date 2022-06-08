5a1c2b2d4ce6804feb18cf6b20deb9ca677d8d11
Slobodan Kustrimovic <skustrimovic@gmail.com>
2022-06-08 17:58:25 +0200

    Temporarily "disable" pagination for subscription management
    A large number to "disable" pagination until improvement to styling are made.

--- src/templates/author.php
+++ src/templates/author.php
@@ -67,7 +67,7 @@ echo "<p>" . wp_kses( $message, wp_kses_allowed_html( 'post' ) ) . "</p>";
             );
 if ( is_array( $subscriptions ) && ! empty( $subscriptions ) ) {
     $total_subscriptions       = count( $subscriptions );
-    $subscriptions_per_page    = 20;
+    $subscriptions_per_page    = 200;
     $subscriptions_total_pages = ceil( $total_subscriptions / $subscriptions_per_page );
     $subscriptions_pagenum     = isset( $_REQUEST['subscription_paged'] ) ? absint( $_REQUEST['subscription_paged'] ) : 1; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
     $subscriptions_offset      = ( $subscriptions_pagenum - 1 ) * $subscriptions_per_page;
